238b4c5a9fa89a14b815990ed4da652ff294b203
HOARAU <Mathieuhoarau@laposte.net>
2021-10-08 09:45:10 +0200

    end

--- Projects/worms/java_project/src/main/java/org/wcscda/worms/board/weapons/ShotgunAmmo.java
+++ Projects/worms/java_project/src/main/java/org/wcscda/worms/board/weapons/ShotgunAmmo.java
@@ -1,13 +1,22 @@
 package org.wcscda.worms.board.weapons;
 
 import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.geom.AffineTransform;
 import java.awt.image.ImageObserver;
+
+import javax.swing.ImageIcon;
+
+import org.wcscda.worms.Helper;
 
 public class ShotgunAmmo extends AbstractAmmo {
   private static final int EXPLOSION_RADIUS = 30;
   private static final int EXPLOSION_DAMAGE = 15;
   private static final int HADOKEN_RECT_SIZE = 10;
-
+  private static final String[] imagePath = {
+			"src/resources/weapons/shotgunAmmo.png"
+	};
+	private static final Image[] SUPERGRENADE = new Image[1];
   public ShotgunAmmo(Double angle) {
     super(EXPLOSION_RADIUS, EXPLOSION_DAMAGE);
     createMovableRect(HADOKEN_RECT_SIZE, HADOKEN_RECT_SIZE);
@@ -17,9 +26,24 @@ public class ShotgunAmmo extends AbstractAmmo {
     setInitialPosition();
   }
 
+	private static void initImages() {
+		for (int i = 0; i < imagePath.length; i++) {
+			SUPERGRENADE[i] = new ImageIcon(imagePath[i]).getImage().getScaledInstance(30, 30, 0);
+		}
+	}
+	
   @Override
   public void drawMain(Graphics2D g, ImageObserver io) {
-    // TODO Auto-generated method stub
-
+		if (SUPERGRENADE[0] == null) {
+			initImages();
+		}
+		if (Helper.getActiveWorm().getDirection() < Math.PI / 2) {
+			g.drawImage(SUPERGRENADE[Helper.getClock() % SUPERGRENADE.length], (int) getMovable().getCenterX(), (int) getMovable().getCenterY() - 18, io);
+		} else {
+			AffineTransform trans =
+					AffineTransform.getTranslateInstance(getMovable().getX(), getMovable().getY());
+			trans.scale(-1, 1);
+			g.drawImage(SUPERGRENADE[Helper.getClock() % SUPERGRENADE.length], trans, io);
+		}
   }
 }
--- Projects/worms/java_project/src/main/java/org/wcscda/worms/gamemechanism/Scores.java
+++ Projects/worms/java_project/src/main/java/org/wcscda/worms/gamemechanism/Scores.java
@@ -43,14 +43,14 @@ public class Scores extends AbstractDrawableElement {
 			int lifePlayer = 0;
 
 			for (int j = 0; j < worms.size(); j++) {
-				g.drawString("Player : "+players.get(i).getName(), (int) x, (int) y );
+				g.drawString("      "+players.get(i).getName(), (int) x, (int) y );
 				//int lifeWorm = Helper.getTC().getPlayers().get(i).getWorms().get(j).getLife();
-				g.drawString("Life: " + worms.get(j).getLife(), (int) x + 70,
+				g.drawString("  " + worms.get(j).getLife(), (int) x +20,
 						(int) y + ligne);
 				ligne += 20;
 				lifePlayer += worms.get(j).getLife();
 			}
-			g.drawString("Total : " + lifePlayer, (int) x + 70,
+			g.drawString("Total : " + lifePlayer, (int) x + 20,
 					(int) y + ligne);
 			x += 140;
 			ligne = 20;
