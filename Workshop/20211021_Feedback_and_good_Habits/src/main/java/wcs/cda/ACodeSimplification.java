08092a7a4929a05988f4c5d59c969dc0fb5f71ab
mister45 <misterdev45@gmail.com>
2021-10-21 12:25:25 +0200

    rewrite methode hasLegalAge, getGroup in operator ternair

--- Workshop/20211021_Feedback_and_good_Habits/src/main/java/wcs/cda/ACodeSimplification.java
+++ Workshop/20211021_Feedback_and_good_Habits/src/main/java/wcs/cda/ACodeSimplification.java
@@ -4,22 +4,25 @@ public class ACodeSimplification {
     // Instruction : rewrite this function in one line
     // WITHOUT using ternary operator
     public static boolean hasLegalAge(int age) {
-        if(age >= 18) {
-            return true;
-        }
-        return false;
+    	//
+    	if(age >= 18) return true; else return false;
+        //with using ternary operator
+        //return age >= 18 ? true : false;
     }
 
     // Instruction : rewrite this function in one line
     // WITH ternary operator
     public static char studentGroup(int number) {
-        char result;
+       //
+    	return number % 2 == 0 ? 'A' : 'B'; 
+    	//
+    	/*char result;
         if(number % 2 == 0) {
             result = 'A';
         }
         else {
             result = 'B';
         }
-        return result;
+        return result;*/
     }
 }
